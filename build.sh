058009bd2e0bdcc02ed8d3521fb078cae25695ab
ziad <ziadk@gmail.com>
2025-08-08 10:12:22 +0300

    add3

--- build.sh
+++ build.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
-sudo apt update
-echo "install dotnet"
-sudo apt install -y aspnetcore-runtime-6.0
-sudo apt install -y dotnet-sdk-6.0
-echo "insatll git"
-sudo apt install git
-git --version
-sudo apt install unzip
-echo "install cli"
+set -e
 
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
-    
-#clone repo from code commit
+# Update packages
+apt update -y
+
+# Install .NET
+echo "Installing .NET..."
+apt install -y aspnetcore-runtime-6.0 dotnet-sdk-6.0
+
+# Install Git & unzip
+echo "Installing Git & unzip..."
+apt install -y git unzip
+
+# Install AWS CLI v2
+echo "Installing AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# Clone GitHub repo
+echo "Cloning repo..."
 cd /home/ubuntu
-echo "git clone"
 sudo -u ubuntu git clone https://github.com/ziad3704/HTTP.git
-cd srv-02echo "dotnet build"
+
+# Build .NET project
+echo "Building project..."
+cd HTTP
 echo 'DOTNET_CLI_HOME=/temp' >> /etc/environment
 export DOTNET_CLI_HOME=/temp
 dotnet publish -c Release --self-contained=false --runtime linux-x64
+
+# Create systemd service
 cat >/etc/systemd/system/srv-02.service <<EOL
 [Unit]
 Description=Dotnet S3 info service
+After=network.target
 
 [Service]
-ExecStart=/usr/bin/dotnet /home/ubuntu/srv-02/bin/Release/netcoreapp6/linux-x64/srv02.dll
+ExecStart=/usr/bin/dotnet /home/ubuntu/HTTP/bin/Release/net6.0/linux-x64/HTTP.dll
 SyslogIdentifier=srv-02
-
 Environment=DOTNET_CLI_HOME=/temp
+Restart=always
+User=ubuntu
+WorkingDirectory=/home/ubuntu/HTTP
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
+# Enable and start service
 systemctl daemon-reload
-
-#run it
+systemctl enable srv-02
 systemctl start srv-02
