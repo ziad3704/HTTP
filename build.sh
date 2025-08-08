909efe6f7816ba12cf785a37d056f4ced169589f
ziad <ziadk@gmail.com>
2025-08-08 12:45:32 +0300

    final edit

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 sudo apt update
 echo "install dotnet"
+wget https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo add-apt-repository ppa:dotnet/backports
 sudo apt install -y aspnetcore-runtime-6.0
 sudo apt install -y dotnet-sdk-6.0
+
 echo "insatll git"
 sudo apt install git
+sudo apt update
+sudo apt install -y aspnetcore-runtime-6.0
 git --version
 sudo apt install unzip
 echo "install cli"
+
 
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
@@ -17,25 +24,31 @@ sudo ./aws/install
 cd /home/ubuntu
 echo "git clone"
 sudo -u ubuntu git clone https://github.com/ziad3704/HTTP.git
+sudo chown -R ubuntu:ubuntu /home/ubuntu/HTTP
+
 cd HTTP
 echo 'DOTNET_CLI_HOME=/temp' >> /etc/environment
+sudo mkdir -p /temp/.nuget/NuGet
+sudo chown -R ubuntu:ubuntu /temp
 export DOTNET_CLI_HOME=/temp
+
 dotnet publish -c Release --self-contained=false --runtime linux-x64
-cat >/etc/systemd/system/srv-02.service <<EOL
+
+
+sudo tee /etc/systemd/system/srv-02.service > /dev/null <<'EOL'
 [Unit]
 Description=Dotnet S3 info service
 
 [Service]
-ExecStart=/usr/bin/dotnet /home/ubuntu/srv-02/bin/Release/netcoreapp6/linux-x64/srv02.dll
+ExecStart=/usr/bin/dotnet /home/ubuntu/HTTP/bin/Release/netcoreapp6/linux-x64/srv02.dll
 SyslogIdentifier=srv-02
-
+Restart=always
 Environment=DOTNET_CLI_HOME=/temp
 
 [Install]
 WantedBy=multi-user.target
 EOL
-
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 #run it
-systemctl start srv-02
+sudo systemctl start srv-02
